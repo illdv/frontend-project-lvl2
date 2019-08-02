a0af44b33e2c76fdc4f0d694562daddfeb9ef954
Kirill Dvoynikov <illdve16@gmail.com>
2019-08-02 20:15:39 +0300

    add first test

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import commander from 'commander';
 import { version, description } from '../../package.json';
+import gendiff from '..';
 
 const program = commander;
 
@@ -8,6 +9,8 @@ program
   .description(`${description}`)
   .arguments('<firstConfig> <secondConfig>')
   .version(`${version}`, '-v, --version')
-  .option('-f, --format [type]', 'Output format');
-
+  .option('-f, --format [type]', 'Output format')
+  .action((file1, file2) => gendiff(file1, file2));
 program.parse(process.argv);
+console.log(__dirname);
+gendiff('/__tests__/__fixtures__/after.json', '/__tests__/__fixtures__/before.json');
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import fs from 'fs';
+
+
+export default (file1, file2) => {
+  console.log(file1, file2);
+};
