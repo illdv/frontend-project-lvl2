4e3e3cf92ff62c6f43e97bde165cac54010b330f
Kirill Dvoynikov <illdve16@gmail.com>
2019-08-29 17:19:19 +0300

    refactor after review 3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+import fs from 'fs';
+import path from 'path';
 import parse from './parsers';
 import buildAst from './buildAst';
 import render from './formatters';
 
+const getContents = (path1, path2) => [path1, path2]
+  .map(currentPath => ({
+    body: fs.readFileSync(currentPath, { encoding: 'utf-8' }),
+    extension: path.extname(currentPath).substring(1),
+  }));
+
+
 export default (pathToFile1, pathToFile2, format) => {
-  const data1 = parse(pathToFile1);
-  const data2 = parse(pathToFile2);
+  const contents = getContents(pathToFile1, pathToFile2);
+  const [data1, data2] = parse(contents);
   const ast = buildAst(data1, data2);
   return render(format, ast);
 };
--- src/parsers.js
+++ src/parsers.js
@@ -1,5 +1,3 @@
-import fs from 'fs';
-import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
@@ -9,8 +7,4 @@ const formats = {
   ini: ini.parse,
 };
 
-export default (filePath) => {
-  const extension = path.extname(filePath).substring(1);
-  const contentFile = fs.readFileSync(filePath, { encoding: 'utf-8' });
-  return formats[extension](contentFile);
-};
+export default contents => contents.map(({ extension, body }) => formats[extension](body));
